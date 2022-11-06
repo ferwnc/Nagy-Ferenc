facd106e995d59d6d6f9062c775be73c1edf1293
Nagy Ferenc <frnc.nagy02@gmail.com>
2022-11-06 21:50:06 +0100

    adatbazis

--- app/Http/Controllers/rogzit.php
+++ app/Http/Controllers/rogzit.php
@@ -8,5 +8,38 @@ use Illuminate\Support\facades\DB;
 
 class rogzit extends Controller
 {
-    //
+
+   public function getContent(){
+    
+            $adatatok = DB::select("SELECT * FROM adat ORDER BY id ASC");
+    
+            return view("adat",["adat" => $adatatok]);
+        }
+        public function adat(Request $req){
+            $req->validate(
+                [
+                    "vnev" => "required|text",
+                    "knev" => "required|text",
+                    "szev" => "required|numeric"
+                ],
+                
+                [
+                    "vnev.required" => "Add meg a vezetékneved!",
+                    "vnev.text" => "Csak szöveget írhatsz",
+                    "knev.required" => "Add meg a keresztneved!",
+                    "knev.text" => "Csak szöveget írhatsz",
+                    "szev.required" => "Add meg a születési éved!",
+                    "szev.numeric" => "Csak számot írhatsz"
+    
+                ]
+            );
+            DB::insert("INSERT INTO adat(id, vnev, knev, szev) VALUES (?,?,?,?)", 
+            [$req->get('id'),
+            $req->get('vnev'),
+            $req->get('knev'),
+            $req->get('szev'),
+            ]);
+    
+            return redirect("/rogzites")->with("success","Az adatokat sikeresen rögzítettük!");
+        }
 }
