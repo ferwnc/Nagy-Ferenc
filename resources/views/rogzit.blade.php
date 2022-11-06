74d97ae7cae22a34a639c3ccc3941a1bb6da9baa
Nagy Ferenc <frnc.nagy02@gmail.com>
2022-11-06 20:24:19 +0100

    form letrehozasa

--- resources/views/rogzit.blade.php
+++ resources/views/rogzit.blade.php
@@ -1,7 +1,18 @@
-<div class="container mt-10">
+<div class="container bg-success text-white">
     <div class="row">
         <div class="col-12">
-            <h1>Töltse ki az űrlapot</h1>
+            <h1>Töltse ki az űrlapot!</h1>
         </div>
     </div>
 </div>  
+
+<div class="container bg-success text-white">
+    <div class="row">
+        <div class="col-12">
+            <form method="POST">
+                @csrf
+
+            </form>
+        </div>
+    </div>
+</div>
